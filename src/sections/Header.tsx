555e598091b045f69010ca40c9636c49cbba4298
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-02 19:11:58 -0300

    Header
    - Logo added
    - Burger added
    - Styles added

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,26 @@
 const Header = () => {
-  return <header>Header</header>
+  return (
+    <header className="sticky top-0 bg-zinc-900/50 backdrop-blur-lg">
+      <div className="container">
+        <div className="flex h-24 items-center justify-between">
+          <div>
+            <img src="/assets/images/logo.svg" alt="Blockforge Logo" />
+          </div>
+
+          <div>
+            <div className="relative size-10">
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="h-0.5 w-5 -translate-y-1 bg-zinc-300"></div>
+              </div>
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="h-0.5 w-5 translate-y-1 bg-zinc-300"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
