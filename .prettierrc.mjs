222eea924fbffac1c97638c2193020f5f579fdbc
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-02 19:09:40 -0300

    Prettier tailwindcss plugin installed and configuration added

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,8 +1,10 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   semi: false,
+  tailwindConfig: "./tailwind.config.mjs",
+  tailwindAttributes: ["className"],
   overrides: [
     {
       files: "*.astro",
