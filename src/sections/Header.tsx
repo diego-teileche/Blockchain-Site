efde5a51e64b1c538f67c88ed8f3ba6198e0c9f5
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-03 19:19:54 -0300

    Header | Button component added

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,5 @@
+import CutCornerButton from "@/components/CutCornerButton"
+
 const Header = () => {
   return (
     <header className="sticky top-0 bg-zinc-900/50 backdrop-blur-lg">
@@ -6,30 +8,15 @@ const Header = () => {
           <div>
             <img
               src="/logo.avif"
-              className="size-14 drop-shadow-lg"
+              className="drop-shadow-lg sm:size-10 md:size-14"
               alt="Blockforge Logo"
             />
           </div>
 
           <div className="flex items-center gap-4">
-            <button className="relative hidden bg-fuchsia-500/20 px-4 py-2 font-heading text-sm font-extrabold uppercase tracking-wide [mask-image:linear-gradient(225deg,transparent,transparent_8px,black_8px)] md:inline-flex">
-              <div className="absolute inset-0 outline outline-2 -outline-offset-2 outline-fuchsia-500 [mask-image:linear-gradient(220deg,transparent,transparent_10px,black_10px)]"></div>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute right-0 top-0 text-fuchsia-500"
-              >
-                <path
-                  d="M0 1H12.2667L23 11.7333V24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                ></path>
-              </svg>
-              <span className="leading-6">Get Started</span>
-            </button>
+            <CutCornerButton className="hidden md:inline-flex">
+              Get Started
+            </CutCornerButton>
             <div className="relative size-10">
               <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
                 <div className="h-0.5 w-5 -translate-y-1 bg-zinc-300"></div>
