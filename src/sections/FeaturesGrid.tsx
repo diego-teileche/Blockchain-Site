45be75cf1db8f5e18c031a4119c1cb78ec4623d2
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-04 19:50:10 -0300

    Features Grid - 2nd Part | Styles added

--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -11,46 +11,51 @@ const FeaturesGrid = () => {
   return (
     <section className="py-24">
       <div className="container">
-        <div>
-          <h2 className="font-heading text-4xl font-black">
-            Empowering the future of blackchain
-          </h2>
-          <p className="mt-8 text-xl text-zinc-400">
-            Blockforge provides robust and secure infrastructure to support the
-            next generation of decentralized applications
-          </p>
-          <ul className="mt-12 flex flex-col gap-8">
-            {listItems.map((item) => (
-              <li key={item} className="flex items-center gap-3">
-                <div className="inline-flex size-8 flex-shrink-0 items-center justify-center rounded-full outline outline-4 -outline-offset-4 outline-fuchsia-500/10">
-                  <div className="size-1.5 rounded-full bg-fuchsia-500"></div>
-                </div>
-                <span className="text-xl font-bold">{item}</span>
-              </li>
-            ))}
-          </ul>
-          <div className="mt-12 flex gap-8">
-            <CutCornerButton>Get Started</CutCornerButton>
-            <TextButton>Learn More</TextButton>
-          </div>
-        </div>
-
-        <div>
-          <h2>Blockforge leads the way</h2>
-          <p>
-            Blockforge is dedicated to supporting the evolution of Web3
-            applications by delivering the necessary infrastructure and security
-            for Web3
-          </p>
-          <p>
-            Blockforge champtions Web3 for everyone. As a decentralized
-            blockchain scaling platform, Blockforge enables developers to create
-            scalable, user-friendly dApps with low transaction costs, all while
-            ensuring robust security
-          </p>
+        <div className="flex flex-col gap-56">
           <div>
-            <CutCornerButton>Get Started</CutCornerButton>
-            <TextButton>Learn More</TextButton>
+            <h2 className="font-heading text-4xl font-black">
+              Empowering the future of blackchain
+            </h2>
+            <p className="mt-8 text-xl text-zinc-400">
+              Blockforge provides robust and secure infrastructure to support
+              the next generation of decentralized applications
+            </p>
+            <ul className="mt-12 flex flex-col gap-8">
+              {listItems.map((item) => (
+                <li key={item} className="flex items-center gap-3">
+                  <div className="inline-flex size-8 flex-shrink-0 items-center justify-center rounded-full outline outline-4 -outline-offset-4 outline-fuchsia-500/10">
+                    <div className="size-1.5 rounded-full bg-fuchsia-500"></div>
+                  </div>
+                  <span className="text-xl font-bold">{item}</span>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-12 flex gap-8">
+              <CutCornerButton>Get Started</CutCornerButton>
+              <TextButton>Learn More</TextButton>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-heading text-4xl font-black">
+              Blockforge leads the way
+            </h2>
+            <div className="mt-6 flex flex-col gap-6 text-xl text-zinc-400">
+              <p>
+                Blockforge is dedicated to supporting the evolution of Web3
+                applications by delivering the necessary infrastructure and
+                security for Web3
+              </p>
+              <p>
+                Blockforge champions Web3 for everyone. As a decentralized
+                blockchain scaling platform, Blockforge enables developers to
+                create scalable, user-friendly dApps with low transaction costs,
+                all while ensuring robust security
+              </p>
+            </div>
+            <div className="mt-12 flex gap-8">
+              <CutCornerButton>Get Started</CutCornerButton>
+              <TextButton>Learn More</TextButton>
+            </div>
           </div>
         </div>
       </div>
