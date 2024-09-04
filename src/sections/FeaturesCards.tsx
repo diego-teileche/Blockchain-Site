b7833d02e9e757aef0d410e981be056dbdb12dd4
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-04 13:20:16 -0300

    Features Cards | Styles added

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -1,28 +1,43 @@
 const FeaturesCards = () => {
   return (
-    <section>
+    <section className="py-24">
       <div className="container">
-        <h2>Discover the future of blockchain with Blockforge</h2>
-        <div>
-          <div>
-            <img
-              src="/assets/images/pill.png"
-              alt="Pill Image | Blockchain Site - Diego Tech"
-            />
-            <h3>Revolutionary Blockchain API</h3>
-            <p>
+        <h2 className="text-center font-heading text-4xl font-black">
+          Discover the future of blockchain with Blockforge
+        </h2>
+        <div className="mt-36">
+          <div className="group relative z-0 max-w-xs p-8">
+            <div className="absolute right-1.5 top-1.5 -z-10 size-16 rounded-xl bg-fuchsia-500 opacity-0 blur-lg transition duration-300 group-hover:opacity-100"></div>
+            <div className="absolute right-1.5 top-1.5 -z-10 size-16 rounded-xl bg-fuchsia-500 transition duration-300 group-hover:bg-fuchsia-400"></div>
+            <div className="absolute inset-0 -z-10 rounded-2xl bg-zinc-800 [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
+            <div className="-mt-28 flex justify-center">
+              <div className="relative inline-flex">
+                <div className="absolute top-[calc(100%+16px)] h-4 w-full rounded-[100%] bg-zinc-950/70 transition duration-300 [mask-image:radial-gradient(closest-side,black,transparent)] group-hover:bg-zinc-950/50"></div>
+                <img
+                  src="/assets/images/pill.png"
+                  className="size-40 transition duration-300 group-hover:-translate-y-4"
+                  alt="Pill Image | Blockchain Site - Diego Tech"
+                />
+              </div>
+            </div>
+            <h3 className="mt-12 font-heading text-3xl font-black">
+              Revolutionary Blockchain API
+            </h3>
+            <p className="mt-4 text-lg text-zinc-400">
               Effortlessly integrate and manage blockchain dara with our
-              cutting-edge API, designed for seamless activity
+              cutting-edge API, designed for seamless connectivity
             </p>
-            <div className="flex">
-              <button>Learn More</button>
+            <div className="mt-12 flex justify-between">
+              <button className="font-heading text-sm font-extrabold uppercase tracking-wider text-fuchsia-500">
+                Learn More
+              </button>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth={1.5}
+                strokeWidth={2}
                 stroke="currentColor"
-                className="size-6"
+                className="size-8 -translate-x-2 text-zinc-500 transition duration-300 group-hover:translate-x-0 group-hover:text-zinc-300"
               >
                 <path
                   strokeLinecap="round"
@@ -34,10 +49,13 @@ const FeaturesCards = () => {
           </div>
         </div>
 
-        <div>
-          <div>
+        <div className="flex justify-center">
+          <div className="inline-flex gap-4 rounded-full bg-zinc-950 p-2.5">
             {[...new Array(4)].fill(0).map((_, i) => (
-              <div key={i}></div>
+              <div
+                key={i}
+                className="size-2.5 cursor-pointer rounded-full bg-zinc-500"
+              ></div>
             ))}
           </div>
         </div>
