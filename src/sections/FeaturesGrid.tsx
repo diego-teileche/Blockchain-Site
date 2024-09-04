7743bbe7f4b68b8cf6d41891e3ecda6382701115
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-04 19:42:32 -0300

    Features Grid - 2nd Part | Elements
    - Title added
    - Paragraphs added
    - Buttons components added

--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -34,6 +34,25 @@ const FeaturesGrid = () => {
             <TextButton>Learn More</TextButton>
           </div>
         </div>
+
+        <div>
+          <h2>Blockforge leads the way</h2>
+          <p>
+            Blockforge is dedicated to supporting the evolution of Web3
+            applications by delivering the necessary infrastructure and security
+            for Web3
+          </p>
+          <p>
+            Blockforge champtions Web3 for everyone. As a decentralized
+            blockchain scaling platform, Blockforge enables developers to create
+            scalable, user-friendly dApps with low transaction costs, all while
+            ensuring robust security
+          </p>
+          <div>
+            <CutCornerButton>Get Started</CutCornerButton>
+            <TextButton>Learn More</TextButton>
+          </div>
+        </div>
       </div>
     </section>
   )
