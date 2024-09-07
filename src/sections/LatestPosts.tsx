2f07e61119c2b813b977964f806873b72364783d
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-07 16:44:37 -0300

    Latest Posts | Elements
    - Title added
    - Paragraph added
    - Cards added
    - Tag, title, paragraph and button for the cards content added

---
+++ src/sections/LatestPosts.tsx
@@ -0,0 +1,32 @@
+const LatestPosts = () => {
+  return (
+    <section>
+      <div className="container">
+        <h2>Your portal to everything blockchain</h2>
+        <p>
+          Keep up with the newest trends, updates, and insights in the
+          blockchain world, updated weekly
+        </p>
+
+        <div>
+          {[...new Array(4)].fill(0).map((item, itemIndex) => (
+            <div key={itemIndex}>
+              <div>Technology</div>
+              <h3>Regulatory Challenges Facing Blockchain</h3>
+              <p>
+                Understanding the regulatory landscape surronding blockchain and
+                what it means for the future of this technology
+              </p>
+              <div>
+                <button>Read more</button>
+                <div>arrow</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default LatestPosts
