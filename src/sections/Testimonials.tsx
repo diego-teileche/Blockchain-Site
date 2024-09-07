4d20ae4fd987f1124876a8cfcaf2d156d53b3e96
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-07 19:42:39 -0300

    Testimonials | Styles added

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,30 +1,33 @@
 const Testimonials = () => {
   return (
-    <section>
+    <section className="bg-zinc-800 py-32">
       <div className="container">
-        {[...new Array(3)].fill(0).map((_, index) => (
-          <div key={index}>
-            <blockquote>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Cumque, aut fugit voluptatum omnis excepturi reiciendis commodi
-                explicabo corporis at sunt
+        <div className="grid grid-cols-1 gap-16">
+          {[...new Array(3)].fill(0).map((_, index) => (
+            <blockquote key={index}>
+              <p className="font-heading text-3xl font-black">
+                &ldquo; Lorem, ipsum dolor sit amet consectetur adipisicing
+                elit. Cumque, aut fugit voluptatum omnis excepturi reiciendis
+                commodi explicabo corporis at sunt &rdquo;
               </p>
-              <cite>
-                <div>
+              <cite className="mt-8 block">
+                <div className="flex items-center gap-3">
                   <div>
-                    <div>avatar</div>
+                    <div className="size-16 rounded-full bg-zinc-700"></div>
                   </div>
-
                   <div>
-                    <div>Diego Tech</div>
-                    <div>React Developer - BlockLink</div>
+                    <div className="text-lg font-black not-italic">
+                      Diego Tech
+                    </div>
+                    <div className="not-italic text-zinc-400">
+                      React Developer - BlockLink
+                    </div>
                   </div>
                 </div>
               </cite>
             </blockquote>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   )
