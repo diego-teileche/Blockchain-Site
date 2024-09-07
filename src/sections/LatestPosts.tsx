f47a7317a35bcc2a8357a7f4f5d97f5cb29957c1
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-07 19:18:18 -0300

    Latest Posts | Button and Responsive
    - Cut corner button added
    - More styles added
    - Responsive design styles added

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -2,6 +2,8 @@ import Card from "@components/Card"
 import type { CollectionEntry } from "astro:content"
 import { getPostColorFromCategory } from "@utils/postUtils"
 import Tag from "@components/Tag"
+import CutCornerButton from "@components/CutCornerButton"
+import { twMerge } from "tailwind-merge"
 
 const LatestPosts = (props: { latestPosts: CollectionEntry<"blog">[] }) => {
   const { latestPosts } = props
@@ -9,30 +11,66 @@ const LatestPosts = (props: { latestPosts: CollectionEntry<"blog">[] }) => {
   return (
     <section className="py-60">
       <div className="container">
-        <h2 className="text-center font-heading text-4xl font-black">
-          Your portal to everything blockchain
-        </h2>
-        <p className="mt-8 text-center text-xl text-zinc-400">
-          Keep up with the newest trends, updates, and insights in the
-          blockchain world, updated weekly
-        </p>
+        <div className="mx-auto max-w-3xl">
+          <h2 className="text-center font-heading text-4xl font-black md:text-5xl lg:text-6xl">
+            Your portal to everything blockchain
+          </h2>
+          <p className="mt-8 text-center text-xl text-zinc-400 lg:text-2xl">
+            Keep up with the newest trends, updates, and insights in the
+            blockchain world, updated weekly
+          </p>
+        </div>
 
-        <div className="mt-16 flex flex-col gap-8">
-          {latestPosts.map(
-            ({ data: { title, description, category } }, postIndex) => (
-              <Card
-                key={postIndex}
-                buttonText="Read More"
-                color={getPostColorFromCategory(category)}
-              >
-                <Tag color={getPostColorFromCategory(category)}>{category}</Tag>
-                <h3 className="mt-4 font-heading text-3xl font-black">
-                  {title}
-                </h3>
-                <p className="mt-6 text-lg text-zinc-400">{description}</p>
-              </Card>
-            ),
-          )}
+        <div className="mt-16 grid grid-cols-1 gap-8 md:mt-28 md:grid-cols-2">
+          <div className="flex flex-col gap-8">
+            {latestPosts.map(
+              ({ data: { title, description, category } }, postIndex) => (
+                <Card
+                  key={postIndex}
+                  buttonText="Read More"
+                  color={getPostColorFromCategory(category)}
+                  className={twMerge(
+                    (postIndex === 1 || postIndex === 3) && "md:hidden",
+                  )}
+                >
+                  <Tag color={getPostColorFromCategory(category)}>
+                    {category}
+                  </Tag>
+                  <h3 className="mt-3 font-heading text-3xl font-black">
+                    {title}
+                  </h3>
+                  <p className="mt-6 text-lg text-zinc-400">{description}</p>
+                </Card>
+              ),
+            )}
+          </div>
+
+          <div className="mt-16 hidden flex-col gap-8 md:flex">
+            {latestPosts.map(
+              ({ data: { title, description, category } }, postIndex) => (
+                <Card
+                  key={postIndex}
+                  buttonText="Read More"
+                  color={getPostColorFromCategory(category)}
+                  className={twMerge(
+                    (postIndex === 0 || postIndex === 2) && "md:hidden",
+                  )}
+                >
+                  <Tag color={getPostColorFromCategory(category)}>
+                    {category}
+                  </Tag>
+                  <h3 className="mt-3 font-heading text-3xl font-black">
+                    {title}
+                  </h3>
+                  <p className="mt-6 text-lg text-zinc-400">{description}</p>
+                </Card>
+              ),
+            )}
+          </div>
+        </div>
+
+        <div className="mt-48 flex justify-center md:mt-32">
+          <CutCornerButton>Read the Blog</CutCornerButton>
         </div>
       </div>
     </section>
