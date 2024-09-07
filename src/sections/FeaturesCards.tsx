5fb3a486d822df0203216cf8afa9edd6258f2d17
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-07 17:21:05 -0300

    Features Cards | Card component added

--- src/sections/FeaturesCards.tsx
+++ src/sections/FeaturesCards.tsx
@@ -1,3 +1,4 @@
+import Card from "@components/Card"
 import TextButton from "@components/TextButton"
 import { twMerge } from "tailwind-merge"
 
@@ -42,28 +43,7 @@ const FeaturesCards = () => {
         <div className="mt-36 flex lg:mt-48">
           <div className="flex flex-none gap-8">
             {cardData.map(({ image, title, description, color }) => (
-              <div
-                key={title}
-                className="group relative z-0 max-w-xs p-8 md:max-w-md md:p-10"
-              >
-                <div
-                  className={twMerge(
-                    "absolute right-1.5 top-1.5 -z-10 size-16 rounded-xl bg-fuchsia-500 opacity-0 blur-lg transition duration-300 group-hover:opacity-100",
-                    color === "lime" && "bg-lime-500",
-                    color === "cyan" && "bg-cyan-500",
-                    color === "violet" && "bg-violet-500",
-                  )}
-                ></div>
-                <div
-                  className={twMerge(
-                    "absolute right-1.5 top-1.5 -z-10 size-16 rounded-xl bg-fuchsia-500 transition duration-300 group-hover:bg-fuchsia-400",
-                    color === "lime" && "bg-lime-500 group-hover:bg-lime-400",
-                    color === "cyan" && "bg-cyan-500 group-hover:bg-cyan-400",
-                    color === "violet" &&
-                      "bg-violet-500 group-hover:bg-violet-400",
-                  )}
-                ></div>
-                <div className="absolute inset-0 -z-10 rounded-2xl bg-zinc-800 [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
+              <Card key={title} className="max-w-xs md:max-w-md">
                 <div className="-mt-28 flex justify-center">
                   <div className="relative inline-flex">
                     <div className="absolute top-[calc(100%+16px)] h-4 w-full rounded-[100%] bg-zinc-950/70 transition duration-300 [mask-image:radial-gradient(closest-side,black,transparent)] group-hover:bg-zinc-950/50"></div>
@@ -78,24 +58,7 @@ const FeaturesCards = () => {
                   {title}
                 </h3>
                 <p className="mt-4 text-lg text-zinc-400">{description}</p>
-                <div className="mt-12 flex justify-between">
-                  <TextButton color={color}>Learn More</TextButton>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    stroke="currentColor"
-                    className="size-8 -translate-x-2 text-zinc-500 transition duration-300 group-hover:translate-x-0 group-hover:text-zinc-300"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
-                    />
-                  </svg>
-                </div>
-              </div>
+              </Card>
             ))}
           </div>
         </div>
