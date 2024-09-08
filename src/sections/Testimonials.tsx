051ae5926522a71559a500b1837cf0f7688a242e
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-07 21:08:10 -0300

    Testimonials | Content and Responsive
    - Content objects created and added
    - More styles added
    - Responsive design styles added

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,26 +1,57 @@
+import { twMerge } from "tailwind-merge"
+
+const testimonials = [
+  {
+    text: "The user experience is phenomenal, and the support team is always there to help. Highly recommended!",
+    name: "Erica Wyatt",
+    title: "Product Manager - BlockLink",
+    avatarImage: "/assets/images/avatar-erica-wyatt.jpg",
+  },
+  {
+    text: "Our productivity has skyrocketed since we started using Blockforge. It's a game-changer for our team",
+    name: "Noel Baldwin",
+    title: "Lead Developer - BitBridge",
+    avatarImage: "/assets/images/avatar-noel-baldwin.jpg",
+  },
+  {
+    text: "The integration process was seamless, and the performance improvements are beyond our expectations",
+    name: "Harry Bender",
+    title: "CTO - CryptoSolutions",
+    avatarImage: "/assets/images/avatar-harry-bender.jpg",
+  },
+]
+
 const Testimonials = () => {
   return (
     <section className="bg-zinc-800 py-32">
       <div className="container">
-        <div className="grid grid-cols-1 gap-16">
-          {[...new Array(3)].fill(0).map((_, index) => (
-            <blockquote key={index}>
-              <p className="font-heading text-3xl font-black">
-                &ldquo; Lorem, ipsum dolor sit amet consectetur adipisicing
-                elit. Cumque, aut fugit voluptatum omnis excepturi reiciendis
-                commodi explicabo corporis at sunt &rdquo;
+        <div className="grid grid-cols-1 gap-16 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-12">
+          {testimonials.map((testimonial, testimonialIndex) => (
+            <blockquote
+              key={testimonialIndex}
+              className={twMerge(
+                testimonialIndex === 2 && "md:hidden lg:block",
+              )}
+            >
+              <p className="font-heading text-3xl font-black lg:text-4xl">
+                &ldquo; {testimonial.text} &rdquo;
               </p>
               <cite className="mt-8 block">
                 <div className="flex items-center gap-3">
                   <div>
-                    <div className="size-16 rounded-full bg-zinc-700"></div>
+                    <div
+                      className="size-16 rounded-full bg-zinc-700 bg-cover"
+                      style={{
+                        backgroundImage: `url(${testimonial.avatarImage})`,
+                      }}
+                    ></div>
                   </div>
                   <div>
                     <div className="text-lg font-black not-italic">
-                      Diego Tech
+                      {testimonial.name}
                     </div>
                     <div className="not-italic text-zinc-400">
-                      React Developer - BlockLink
+                      {testimonial.title}
                     </div>
                   </div>
                 </div>
