9833ee7c219b5d13a13ae6e927d5f9ca1b172f6d
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-10 13:44:31 -0300

    Call to Action | Basic styles added

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -2,11 +2,17 @@ import CutCornerButton from "@components/CutCornerButton"
 
 const CallToAction = () => {
   return (
-    <section>
+    <section className="py-60">
       <div className="container">
-        <h2>Ready to get started?</h2>
-        <p>Start building using blockchain technology, with Blockforge</p>
-        <CutCornerButton>Get Started</CutCornerButton>
+        <h2 className="text-center font-heading text-4xl font-black">
+          Ready to <span className="block">get started?</span>
+        </h2>
+        <p className="mt-8 text-center text-xl text-zinc-400">
+          Start building using blockchain technology, with Blockforge
+        </p>
+        <div className="mt-12 flex justify-center">
+          <CutCornerButton>Get Started</CutCornerButton>
+        </div>
       </div>
     </section>
   )
