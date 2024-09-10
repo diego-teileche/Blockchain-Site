d9dd814737a63a2c4d3a0f0134e659aef3195817
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-10 13:38:27 -0300

    Call to Action | Elements
    - Title added
    - Paragraph added
    - Cut corner button component added

---
+++ src/sections/CallToAction.tsx
@@ -0,0 +1,15 @@
+import CutCornerButton from "@components/CutCornerButton"
+
+const CallToAction = () => {
+  return (
+    <section>
+      <div className="container">
+        <h2>Ready to get started?</h2>
+        <p>Start building using blockchain technology, with Blockforge</p>
+        <CutCornerButton>Get Started</CutCornerButton>
+      </div>
+    </section>
+  )
+}
+
+export default CallToAction
