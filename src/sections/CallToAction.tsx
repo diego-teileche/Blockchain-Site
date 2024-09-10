9edb1641615de0622a5f081e5ea7796f49e73b54
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-10 14:00:49 -0300

    Call to Action | Images
    - Hexagon component added
    - Circle component added
    - Images added
    - Responsive design styles added
    - More styles added

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,17 +1,47 @@
+import Circle from "@components/Circle"
 import CutCornerButton from "@components/CutCornerButton"
+import Hexagon from "@components/Hexagon"
 
 const CallToAction = () => {
   return (
-    <section className="py-60">
+    <section className="overflow-hidden py-60">
       <div className="container">
-        <h2 className="text-center font-heading text-4xl font-black">
-          Ready to <span className="block">get started?</span>
-        </h2>
-        <p className="mt-8 text-center text-xl text-zinc-400">
-          Start building using blockchain technology, with Blockforge
-        </p>
-        <div className="mt-12 flex justify-center">
-          <CutCornerButton>Get Started</CutCornerButton>
+        <div className="relative">
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Hexagon className="size-[700px]" />
+          </div>
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Hexagon className="size-[1100px]" />
+          </div>
+
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Circle className="absolute -top-[400px] left-0">
+              <img
+                src="/assets/images/cuboid.png"
+                className="size-[140px]"
+                alt="Cuboid Blockforge Site | Diego Tech"
+              />
+            </Circle>
+          </div>
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Circle className="absolute -left-[600px] -top-[70px]">
+              <img
+                src="/assets/images/cylinder.png"
+                className="size-[140px]"
+                alt="Cuboid Blockforge Site | Diego Tech"
+              />
+            </Circle>
+          </div>
+
+          <h2 className="text-center font-heading text-4xl font-black md:text-5xl lg:text-6xl">
+            Ready to <span className="block">get started?</span>
+          </h2>
+          <p className="mx-auto mt-8 max-w-sm text-center text-xl text-zinc-400 lg:text-2xl">
+            Start building using blockchain technology, with Blockforge
+          </p>
+          <div className="mt-12 flex justify-center">
+            <CutCornerButton>Get Started</CutCornerButton>
+          </div>
         </div>
       </div>
     </section>
