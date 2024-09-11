983a1e868a003cfe9a25821c71f78abddf14dcda
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-11 14:52:15 -0300

    Circle component modified and animation added

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,16 +1,35 @@
 import type { ComponentPropsWithoutRef } from "react"
 import { twMerge } from "tailwind-merge"
+import { motion } from "framer-motion"
 
-const Circle = (props: ComponentPropsWithoutRef<"div">) => {
-  const { className, children } = props
+const Circle = (
+  props: ComponentPropsWithoutRef<"div"> & { animate?: boolean },
+) => {
+  const { className, children, animate = false } = props
 
   return (
     <div
       className={twMerge(
-        "inline-flex size-[240px] items-center justify-center rounded-full bg-zinc-900 outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10",
+        "relative inline-flex size-[240px] items-center justify-center rounded-full bg-zinc-900",
         className,
       )}
     >
+      <motion.div
+        animate={
+          animate && {
+            rotate: 360,
+          }
+        }
+        transition={{
+          ease: "linear",
+          duration: 15,
+          repeat: Infinity,
+        }}
+        className={twMerge(
+          "absolute inset-0 rounded-full border-[6px] border-transparent outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10",
+          animate && "border-t-fuchsia-500/30",
+        )}
+      />
       {children}
     </div>
   )
