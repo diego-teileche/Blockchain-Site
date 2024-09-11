7b4fe442edebacd223e1141172db81e7a266edd5
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-10 21:49:39 -0300

    Footer | Elements added

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,33 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div>
+          <div>
+            <img
+              src="/assets/images/logo.svg"
+              alt="Blockforge Logo | Diego Tech"
+            />
+          </div>
+
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">Blog</a>
+            <a href="#">Careers</a>
+            <a href="#">Contact</a>
+          </nav>
+        </div>
+        <div>
+          <p>&copy; 2024 Blockforge. All rights reserved.</p>
+          <div>
+            <div>Discord</div>
+            <div>Discord</div>
+            <div>Discord</div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
