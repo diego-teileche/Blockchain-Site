562fe233b3084e460f52ea8ddb5cf2a305996403
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-11 21:11:11 -0300

    Call to Action | Animations added

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,33 +1,50 @@
 import Circle from "@components/Circle"
 import CutCornerButton from "@components/CutCornerButton"
 import Hexagon from "@components/Hexagon"
+import { motion, useScroll, useTransform } from "framer-motion"
+import { useRef } from "react"
 
 const CallToAction = () => {
+  const sectionRef = useRef(null)
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  })
+
+  const rotate = useTransform(scrollYProgress, [0, 1], [45, -45])
+
   return (
-    <section className="overflow-hidden py-60">
+    <section ref={sectionRef} className="overflow-hidden py-60">
       <div className="container">
         <div className="relative">
           <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Hexagon className="size-[700px]" />
+            <Hexagon size={700} />
           </div>
           <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Hexagon className="size-[1100px]" />
+            <Hexagon size={1100} reverse duration={60} />
           </div>
 
           <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Circle className="absolute -top-[400px] left-0">
-              <img
+            <Circle className="absolute -top-[400px] left-0" animate>
+              <motion.img
                 src="/assets/images/cuboid.png"
                 className="size-[140px]"
+                style={{
+                  rotate,
+                }}
                 alt="Cuboid Blockforge Site | Diego Tech"
               />
             </Circle>
           </div>
           <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Circle className="absolute -left-[600px] -top-[70px]">
-              <img
+            <Circle className="absolute -left-[600px] -top-[70px]" animate>
+              <motion.img
                 src="/assets/images/cylinder.png"
                 className="size-[140px]"
+                style={{
+                  rotate,
+                }}
                 alt="Cuboid Blockforge Site | Diego Tech"
               />
             </Circle>
