c590de8040ac26e956d056b1b20597c062d8a321
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-25 18:42:15 -0300

    Header | Navigation anchors and styles added

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,34 +1,81 @@
 import CutCornerButton from "@components/CutCornerButton"
+import Hexagon from "@components/Hexagon"
+
+const navLinks = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "Careers",
+    href: "/careers",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+]
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-10 bg-zinc-900/50 backdrop-blur-lg">
-      <div className="container">
-        <div className="flex h-16 items-center justify-between md:h-20">
-          <div>
-            <img
-              src="/logo.avif"
-              className="relative sm:size-10 md:size-14"
-              alt="Blockforge Logo"
-            />
-          </div>
+    <>
+      <header className="sticky top-0 z-40 bg-zinc-900/50 backdrop-blur-lg">
+        <div className="container">
+          <div className="flex h-16 items-center justify-between md:h-20">
+            <div>
+              <img
+                src="/logo.avif"
+                className="relative sm:size-10 md:size-14"
+                alt="Blockforge Logo"
+              />
+            </div>
 
-          <div className="flex items-center gap-4">
-            <CutCornerButton className="hidden md:inline-flex">
-              Get Started
-            </CutCornerButton>
-            <div className="relative size-10">
-              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                <div className="h-0.5 w-5 -translate-y-1 bg-zinc-300"></div>
-              </div>
-              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                <div className="h-0.5 w-5 translate-y-1 bg-zinc-300"></div>
+            <div className="flex items-center gap-4">
+              <CutCornerButton className="hidden md:inline-flex">
+                Get Started
+              </CutCornerButton>
+              <div className="relative size-10">
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                  <div className="h-0.5 w-5 -translate-y-1 bg-zinc-300"></div>
+                </div>
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                  <div className="h-0.5 w-5 translate-y-1 bg-zinc-300"></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </header>
+
+      <div className="fixed left-0 top-0 z-30 size-full bg-zinc-900">
+        <div className="absolute inset-2 z-0 mt-20 rounded-md bg-zinc-800">
+          <div className="absolute left-full top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2">
+            <Hexagon size={700} />
+          </div>
+          <div className="absolute left-full top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2">
+            <Hexagon size={1100} />
+          </div>
+
+          <div className="flex h-full items-center justify-center">
+            <nav className="flex flex-col items-center gap-12">
+              {navLinks.map(({ title, href }) => (
+                <a
+                  key={title}
+                  href={href}
+                  className="font-heading text-4xl font-black text-zinc-500 transition-colors duration-300 hover:text-zinc-300"
+                >
+                  {title}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
       </div>
-    </header>
+    </>
   )
 }
 
